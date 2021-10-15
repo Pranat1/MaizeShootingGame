9c7e329d6742744940188ad206db81861250bb48
Pranat1 <pranat@lucknowmarbles.com>
2021-10-15 18:54:46 +0530

    first commit shooting game

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    bool isProvoked = false;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {EngageTarget();}
+        else if (chaseRange >= distanceToTarget){
+        isProvoked = true;
+        }
+    }
+    private void EngageTarget(){
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    
+    private void AttackTarget(){
+        Debug.Log("Cought You");
+    }
+    private void ChaseTarget(){
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
