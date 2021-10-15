71ddf8ac73d9bdc6c3888d2d49ae34267a7c9f44
Pranat1 <pranat@lucknowmarbles.com>
2021-10-15 23:24:56 +0530

    shooting problem

---
+++ Assets/Scripts/EnimyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnimyHealth : MonoBehaviour
+{
+    [SerializeField] private float health = 5f;
+    // Start is called before the first frame update
+
+    public void TakeDamage(float damageAmount){
+        health -= damageAmount;
+        if (health <= 0f){
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{ 
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 1000f;
+    [SerializeField] float damageAmount = 1f;
+    [SerializeField] ParticleSystem muzzleFlash;
+
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    private void Shoot(){
+        //PlayMuzzleFlash();
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log(hit.transform.name);
+        /*
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            EnimyHealth target = hit.transform.GetComponent<EnimyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damageAmount);
+        }
+        else{
+            return;
+        }
+        //ProcessRaycasting();*/
         
+    }
+    private void ProcessRaycasting(){
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            EnimyHealth target = hit.transform.GetComponent<EnimyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damageAmount);
+        }
+        else{
+            return;
+        }
+    }
+    private void PlayMuzzleFlash(){
+        muzzleFlash.Play();
     }
 }
